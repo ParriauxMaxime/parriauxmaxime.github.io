1f58eb8387b5c4fdcef890f11f725a6d28261541
Maxime Parriaux <maxime.parriaux@epitech.eu>
2022-10-24 14:15:07 +0200

    🍎 Improve responsive on higher DPI

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -5,7 +5,7 @@ function Article({ children, id }: { children: JSX.Element; id?: string }) {
     <article id={id} className="relative">
       <div className="absolute top-0 left-0 w-0 h-0 border-l-0 border-r-[55vw] border-x-transparent border-t-[4vh] md:border-t-[8vh] dark:border-t-secondary-dark border-t-secondary duration-500" />
       <div className="relative flex flex-wrap w-full md:items-center justify-around min-h-[100vh] md:min-h-[93vh] md:max-h-80vh m-auto md:max-w-screen py-12 md:py-20 px-6 md:px-8">
-        <div className="flex flex-col w-full md:w-[80vw] lg:w-[60vw] 3xl:w-[50vw]">
+        <div className="flex flex-col w-full xl:w-[80vw] 2xl:w-[75vw]">
           {children}
         </div>
       </div>
